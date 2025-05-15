718858535011ac9d8b9fe78f78c741828a275edb
guyscottages@gmail.com <guyscottages@gmail.com>
2025-05-15 10:28:00 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,7 +8,20 @@ import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+   # Set Form properties and Data Bindings.
+   self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+  def submit_button_click(self, **event_args):
+    name = self.name_box.text
+    email = self.email_box.text
+    feedback = self.feedback_box.text
+    anvil.server.call('add_feedback', name, email, feedback)
+    Notification("Feedback submitted!").show()
+    # Call your 'clear_inputs' method to clear the boxes
+    self.clear_inputs()
+
+  def clear_inputs(self):
+    # Clear our three text boxes
+    self.name_box.text = ""
+    self.email_box.text = ""
+    self.feedback_box.text = ""
